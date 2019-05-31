adc17437ab3430ea71951aa06f95e880436165a4
hdsdi3g <admin@hd3g.tv>
2019-06-01 00:24:55 +0200

    Clean and upgrade, remove winrun4j and java scripts, add javappackager tags, add log4j2 conf to log file.

--- make.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-call mvn clean dependency:copy-dependencies package
-
-java win64-ressources\windows-package.java
--- src/test/java/tv/hd3g/jytdl/tools/TestShortcutFileParser.java
+++ src/test/java/tv/hd3g/jytdl/tools/TestShortcutFileParser.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of jYTdl.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -10,9 +10,9 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * Copyright (C) hdsdi3g for hd3g.tv 2018
- * 
+ *
 */
 package tv.hd3g.jytdl.tools;
 
@@ -21,37 +21,43 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.commons.io.FileUtils;
+
 import junit.framework.TestCase;
 
 public class TestShortcutFileParser extends TestCase {
-	
+
 	private final File f_regular_link_desktop;
 	private final File f_video_link_desktop;
 	private final URL url_regular_link_desktop;
 	private final URL url_video_link_desktop;
-	
-	public TestShortcutFileParser() throws MalformedURLException {
-		f_regular_link_desktop = new File(getClass().getClassLoader().getResource("regular-link.desktop").getFile());
-		f_video_link_desktop = new File(getClass().getClassLoader().getResource("video-link.desktop").getFile());
+
+	public TestShortcutFileParser() throws MalformedURLException, IOException {
+		f_regular_link_desktop = File.createTempFile("regular-link", ".desktop");
+		FileUtils.copyInputStreamToFile(getClass().getResource("regular-link.desktop").openStream(), f_regular_link_desktop);
+
+		f_video_link_desktop = File.createTempFile("video-link", ".desktop");
+		FileUtils.copyInputStreamToFile(getClass().getResource("video-link.desktop").openStream(), f_video_link_desktop);
+
 		url_regular_link_desktop = new URL("https://www.youtube.com/user/ScottBradleeLovesYa");
 		url_video_link_desktop = new URL("https://www.youtube.com/watch?v=aLnZ1NQm2uk");
 	}
-	
+
 	public void testURLFile() {
 		// XXX create windows files
 	}
-	
+
 	public void testDesktopFile() throws IOException {
-		ShortcutFileParser fp = new ShortcutFileParser();
+		final ShortcutFileParser fp = new ShortcutFileParser();
 		assertEquals(url_regular_link_desktop, fp.parseFreeDesktopFile(f_regular_link_desktop));
 		assertEquals(url_video_link_desktop, fp.parseFreeDesktopFile(f_video_link_desktop));
 	}
-	
+
 	public void testRoutingType() throws IOException {
-		ShortcutFileParser fp = new ShortcutFileParser();
+		final ShortcutFileParser fp = new ShortcutFileParser();
 		assertEquals(url_regular_link_desktop, fp.getURL(f_regular_link_desktop));
 		assertEquals(url_video_link_desktop, fp.getURL(f_video_link_desktop));
 		// XXX add windows files
 	}
-	
+
 }
